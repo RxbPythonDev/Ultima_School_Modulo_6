c5ca045af3c38ee2a35b4a6b9c13cb71603d776e
RxbPythonDev <rxbpythondev@gmail.com>
2023-08-06 19:12:55 -0300

    Códigos praticados seguindo o conteúdo da plataforma na segunda semana do módulo 6

---
+++ Semana 2/Conteúdo da plataforma/rest_api/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from reserva.models import Reserva
+from rest_api.serializers import AgendamentoModelSerializer
+
+# Create your views here.
+
+class AgendamentoModelViewSet(ModelViewSet):
+    queryset = Reserva.objects.all()
+    serializer_class = AgendamentoModelSerializer
+
+
+@api_view(['GET', 'POST'])
+def hello_world(request):
+    if request.method == 'POST':
+        return Response({'message': f'Hello, {request.data.get("name")}'})
+    
+    return Response({'Hello': 'World API'})
